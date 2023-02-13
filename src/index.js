5db700386fa2eb3a2b8d590a776000e3195a4184
Sergey Lukashevich <rs.lukashevich@gmail.com>
2023-02-13 20:49:38 +0300

    init: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function toReadable (number) {
-  
-}
+    const arr = ['',' one',' two',' three',' four',' five',' six',' seven',' eight',' nine',' ten',' eleven',
+    ' twelve',' thirteen',' fourteen',' fifteen',' sixteen',' seventeen',' eighteen',' nineteen'];
+    const arr2 = ['','',' twenty',' thirty',' forty',' fifty',' sixty',' seventy',' eighty',' ninety']
+    
+    if (number === 0) return 'zero';
+    let strnum = String(number).padStart(3,'0');
+
+    let x  = (strnum[0] === '0') ? '' : arr[strnum[0]] + ' hundred' ;
+        x += (Number(strnum.slice(1)) < 20) ? arr[Number(strnum.slice(1))] : 
+             arr2[strnum[1]] + arr[strnum[2]];
+    return x.trim();
+    }  
